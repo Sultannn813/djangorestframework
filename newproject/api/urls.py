305c1146de911d23dc516f19f4d1c0fe63dea669
Sultannn813 <sultankuanysh813@gmail.com>
2026-08-02 18:29:43 +0500

    добавил еще новые пути

--- newproject/api/urls.py
+++ newproject/api/urls.py
@@ -4,5 +4,8 @@ from .views import get_users, create_users  # <-- Импорт
 
 urlpatterns = [
     path('users/', get_users, name='get_users'),
-    path('users/create/', create_users, name='create_user'),  # <-- Маршрут
+    path('users/create/', create_users, name='create_user'),
+    path('users/<int:pk>/', get_users, name='get_user'),
+    path('users/<int:pk>/update/', get_users, name='update_user'),
+    path('users/<int:pk>/delete/', get_users, name='delete_user')
 ]
